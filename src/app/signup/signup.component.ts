6cd81371dcd8bcd310a7b286d5d2dcaace7a64c5
Guruprasaath <96716307+Gururaagav@users.noreply.github.com>
2022-09-20 23:38:19 +0530

    Add files via upload
    login and signup with toast

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-signup',
@@ -11,22 +15,28 @@ export class SignupComponent implements OnInit {
   dropdown1 = '';
   pass ='';
   cnfpass ='';
+  
 
-  constructor(public router: Router) { }
+  constructor( public router:Router, private toastr:ToastrService ) { }
+
 
   isSame(){
     return this.pass === this.cnfpass;
   }
 
+ 
+
   onSignup(signupForm: any){
     if(signupForm.invalid){
-      alert("Please enter valid data");
+      this.toastr.error('Please check your information','ERROR',{timeOut:3000,progressBar:true, progressAnimation:'increasing'});
     }
     else{
-    alert("Creating new Account");
+    this.toastr.success('Creating the account!!','INFO',{timeOut:3000,progressBar:true,progressAnimation:'increasing'});
     }
   };
 
+
   ngOnInit(): void {
   }
+
 }
