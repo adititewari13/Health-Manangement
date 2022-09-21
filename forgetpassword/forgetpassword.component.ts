5dced2c0801cfb4a516d58d005ea05eafa033b3b
Guruprasaath <96716307+Gururaagav@users.noreply.github.com>
2022-09-21 11:49:34 +0530

    Add files via upload
    forget password with toaster

--- forgetpassword/forgetpassword.component.ts
+++ forgetpassword/forgetpassword.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrModule } from 'ngx-toastr';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-forgetpassword',
@@ -10,15 +13,21 @@ export class ForgetpasswordComponent implements OnInit {
   newpass='';
   cnfpass='';
 
-  constructor() { }
+  constructor(private toastr: ToastrService) { }
 
   isSame(){
     return this.newpass===this.cnfpass;
   }
 
-  FSubmit(){
-    alert('Your password has been successfully reset!!');
+  FSubmit(Forgetpasswordform:any){
+    if(Forgetpasswordform.invalid){
+      this.toastr.error('Please check your information','ERROR',{timeOut:2000,progressBar:true,progressAnimation:'increasing'})
+    }
+    else{
+      this.toastr.success('Your Password has been reset!!','INFO',{timeOut:2000,progressBar:true,progressAnimation:'increasing'})
+    }
   }
+
 
   ngOnInit(): void {
   }
